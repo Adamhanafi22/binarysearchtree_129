a20aa23f3ddb028d52d8e82fe439b607a82c04ed
Adamhanafi22 <adamhanafi1922@gmail.com>
2023-07-02 01:19:44 +0700

    meethoud menampilkan antrian

--- binarysearchtree_129/binarysearchtree_129.cpp
+++ binarysearchtree_129/binarysearchtree_129.cpp
@@ -87,5 +87,11 @@ public:
 			preorder(ptr->rightchild);
 		}
 	}
+	void postorder(Node* ptr) {
+		//Performs the postorder traversal of the tree
+		if (ROOT == NULL) {
+			cout << "Tree is empty" << endl;
+			return;
+		}
 
 };
