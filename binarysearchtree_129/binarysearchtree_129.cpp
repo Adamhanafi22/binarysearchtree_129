c7955fa0693eb9d2c759c63c2446882bce583249
Adamhanafi22 <adamhanafi1922@gmail.com>
2023-07-02 00:59:34 +0700

    methoud

--- binarysearchtree_129/binarysearchtree_129.cpp
+++ binarysearchtree_129/binarysearchtree_129.cpp
@@ -25,5 +25,16 @@ public:
 	Node* ROOT;
 
 	binaryTree()
+	{
+		ROOT = NULL; // initializing ROOT to null
+	}
 
+	void insert(string element) // insert a node in the binary search tree
+	{
+		Node* newNode = new Node(element, NULL, NULL);	//Allocate memory for the new node
+		newNode->info = element;	//Assign value to the data field of the new node
+		newNode->leftchild = NULL;	//Make the left chield of the new node point to NULL
+		newNode->rightchild = NULL; //Make the right child of the new node point to NULL
+
+	}
 };
