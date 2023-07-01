dc5914b7549f02755cdf4cc0b22f72d9b8d58cca
Adamhanafi22 <adamhanafi1922@gmail.com>
2023-07-02 00:48:00 +0700

    membuat node baru

--- binarysearchtree_129/binarysearchtree_129.cpp
+++ binarysearchtree_129/binarysearchtree_129.cpp
@@ -9,4 +9,12 @@ public:
 	Node* leftchild;
 	Node* rightchild;
 
+	// Constructor for the node class
+	Node(string i, Node* l, Node* r)
+	{
+		info = 1;
+		leftchild = l;
+		rightchild = r;
+	}
+
 }; 
