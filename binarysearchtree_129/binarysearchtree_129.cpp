ca0cd6431000473f79fda375b024aa6247a34009
Adamhanafi22 <adamhanafi1922@gmail.com>
2023-07-02 01:09:33 +0700

    nilai bawah nilai atas

--- binarysearchtree_129/binarysearchtree_129.cpp
+++ binarysearchtree_129/binarysearchtree_129.cpp
@@ -2,8 +2,7 @@
 #include <string>
 using namespace std;
 
-class Node
-{
+class Node{
 public:
 	string info;
 	Node* leftchild;
@@ -19,8 +18,7 @@ public:
 
 }; 
 
-class binaryTree
-{
+class binaryTree{
 public:
 	Node* ROOT;
 
@@ -35,6 +33,17 @@ public:
 		newNode->info = element;	//Assign value to the data field of the new node
 		newNode->leftchild = NULL;	//Make the left chield of the new node point to NULL
 		newNode->rightchild = NULL; //Make the right child of the new node point to NULL
+
+		Node* parent = NULL;
+		Node* currentNode = NULL;
+		search(element, parent, currentNode); //Locate the node which will be the parent of the node to be inserted
+
+		if (parent == NULL)  //If the parent is NULL (Tree is empty)
+		{
+			ROOT = newNode;	//Mark the new node as ROOT
+			return; //Exit
+		}
+		if (element < parent->info)	//if the value in the data field of the new node is less than that of the parent
 
 	}
 };
